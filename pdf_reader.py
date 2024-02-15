a688dab2794dd44137891d9a065512c5865a1d67
lombardi-g <gabriellomba@gmail.com>
2024-02-15 09:15:45 -0300

    Example pdf

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,1 +1,8 @@
 from PyPDF2 import PdfReader
+
+match_summary_pdf = "FIGxMAN.pdf"
+reader = PdfReader(match_summary_pdf)
+
+# def extract_from_summary():
+page = reader.pages[0]
+text = page.extract_text()
