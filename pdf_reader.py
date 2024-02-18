73ef139ff221933a0844b9bf6e776cc57f22a27e
lombardi-g <gabriellomba@gmail.com>
2024-02-18 20:21:46 -0300

    Final score information

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,6 +1,6 @@
 from PyPDF2 import PdfReader
 
-match_summary_pdf = "FIGxMAN.pdf"
+match_summary_pdf = "VRExFIG.pdf"
 reader = PdfReader(match_summary_pdf)
 
 def extract_info_after_label(text, label):
@@ -13,9 +13,20 @@ def extract_info_after_label(text, label):
 page = reader.pages[0]
 text = page.extract_text()
 
+# General information
 full_match_date = extract_info_after_label(text, "Data:")
 full_location = extract_info_after_label(text, "Estádio:")
 stadium = full_location.split(" / ")[0]
 city = full_location.split(" / ")[1]
 time = extract_info_after_label(full_match_date, "Horário:")[:5]
 date = full_match_date[:10]
+home = True if stadium =="Orlando Scarpelli" else False
+
+# Score information
+final_score = extract_info_after_label(text, "Resultado Final:").strip().split(" X ")
+if home:
+    fig_final_score = final_score[0]
+    opponent_final_score = final_score[1]
+else:
+    fig_final_score = final_score[1]
+    opponent_final_score = final_score[0]
