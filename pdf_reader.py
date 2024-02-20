05adf00d3ff3a27d7f9b53fb48257a29e379e03e
lombardi-g <gabriellomba@gmail.com>
2024-02-20 10:32:59 -0300

    Renaming var

--- pdf_reader.py
+++ pdf_reader.py
@@ -11,11 +11,12 @@ def extract_info_after_label(text, label):
 
 # def extract_from_summary():
 page = reader.pages[0]
-text = page.extract_text()
+cbf_pdf = page.extract_text()
 
 # General information
-full_match_date = extract_info_after_label(text, "Data:")
-full_location = extract_info_after_label(text, "Estádio:")
+opponent = extract_info_after_label(cbf_pdf, "Jogo:")
+full_match_date = extract_info_after_label(cbf_pdf, "Data:")
+full_location = extract_info_after_label(cbf_pdf, "Estádio:")
 stadium = full_location.split(" / ")[0]
 city = full_location.split(" / ")[1]
 time = extract_info_after_label(full_match_date, "Horário:")[:5]
@@ -23,7 +24,7 @@ date = full_match_date[:10]
 home = True if stadium =="Orlando Scarpelli" else False
 
 # Score information
-final_score = extract_info_after_label(text, "Resultado Final:").strip().split(" X ")
+final_score = extract_info_after_label(cbf_pdf, "Resultado Final:").strip().split(" X ")
 if home:
     fig_final_score = final_score[0]
     opponent_final_score = final_score[1]
@@ -32,8 +33,10 @@ else:
     opponent_final_score = final_score[0]
 
 # Time and additional minutes
-first_half_added_minutes = extract_info_after_label(text, "Acréscimo:").split(" min")[0]
-second_half_added_minutes = extract_info_after_label(extract_info_after_label(text, "Término do 2"),"Acréscimo:").split(" m")[0]
+first_half_added_minutes = extract_info_after_label(cbf_pdf, "Acréscimo:").split(" min")[0]
+second_half_added_minutes = extract_info_after_label(extract_info_after_label(cbf_pdf, "Término do 2"),"Acréscimo:").split(" m")[0]
 first_half_minutes = 45 + int(first_half_added_minutes)
 second_half_minutes = 45 + int(second_half_added_minutes)
 match_minutes = first_half_minutes + second_half_minutes
+
+print(cbf_pdf)
